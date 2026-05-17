6a26e656ed29ccaa5a7861c54664347b7a5084f7
jleprog39 <jleprog39@gmail.com>
2026-05-17 20:50:57 +0300

    Update config.js

--- .dmtools/config.js
+++ .dmtools/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   repository: { owner: 'JuliusAgency', repo: 'polybet' },
-  jira:       { project: 'PB' },
+  jira:       { project: 'PB', parentKey: 'PB-0' },
   git:        { baseBranch: 'main', authorEmail: 'jleprog39@gmail.com' },
   scm:        { provider: 'github' }
 };
